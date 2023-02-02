80c19c71fd204d0ca99609eee0f370ef1e26dfca
Vee Uye <vee.uye@gmail.com>
2023-02-02 21:18:13 +0000

    shameless green: pass first test

---
+++ shamelessGreen/lib/bottles.js
@@ -0,0 +1,14 @@
+import {capitalize, downTo} from './helpers';
+
+class Bottles {
+  verse() {
+    return (
+      '99 bottles of beer on the wall, ' +
+      '99 bottles of beer.\n' +
+      'Take one down and pass it around, '
+      + '98 bottles of beer on the wall.\n'
+    );
+  }
+}
+
+export { Bottles };
